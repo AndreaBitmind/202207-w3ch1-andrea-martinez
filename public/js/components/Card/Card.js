18e1855e891a347edeb2ec0c1157d179d837052a
Andrea Martinez <andrea.maort@gmail.com>
2022-07-18 22:39:39 +0200

    class card created

--- public/js/components/Card/Card.js
+++ public/js/components/Card/Card.js
@@ -1,7 +1,18 @@
 import Component from "../Component.js";
 
 class Card extends Component {
-  constructor() {
-    super();
+  constructor(parent) {
+    super(parent, "characterCol", "li");
+
+    this.render();
+  }
+
+  render() {
+    const characterDiv = document.createElement("div");
+    characterDiv.className = "card character__card";
+
+    this.element.appendChild(characterDiv);
   }
 }
+
+export default Card;

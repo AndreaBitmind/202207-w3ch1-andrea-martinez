14023fafb1e5b8d87cf47e415ec42c822c2ad51b
Andrea Martinez <andrea.maort@gmail.com>
2022-07-18 19:46:45 +0200

    created a new file for every class

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,5 +8,13 @@ module.exports = {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  rules: {},
+  rules: {
+    "no-new": "off",
+    "import/extensions": "off",
+    "lines-between-class-members": [
+      "error",
+      "always",
+      { exceptAfterSingleLine: true },
+    ],
+  },
 };

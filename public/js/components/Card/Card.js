b7197776c6bf4b7f1ab14b0a7ce1a12805ea184e
Andrea Martinez <andrea.maort@gmail.com>
2022-07-18 23:21:15 +0200

    content inside the card added

--- public/js/components/Card/Card.js
+++ public/js/components/Card/Card.js
@@ -10,8 +10,26 @@ class Card extends Component {
   render() {
     const characterDiv = document.createElement("div");
     characterDiv.className = "card character__card";
-
     this.element.appendChild(characterDiv);
+
+    const image = document.createElement("img");
+    image.className = "character__picture card-img-top";
+    image.alt = "Nombre y familia del personaje";
+    image.src = "../../img/bronn.jpg";
+    characterDiv.appendChild(image);
+
+    const divTitle = document.createElement("div");
+    divTitle.className = "card-body";
+    characterDiv.appendChild(divTitle);
+
+    const characterTitle = document.createElement("h2");
+    characterTitle.className = "character__name card-title h4";
+    characterTitle.textContent = "Nombre y familia";
+    divTitle.appendChild(characterTitle);
+
+    const characterInfo = document.createElement("div");
+    characterInfo.className = "character-info";
+    divTitle.appendChild(characterInfo);
   }
 }
 
--- public/js/index.js
+++ public/js/index.js
@@ -2,12 +2,12 @@ import allCharacters from "./components/personajes.js";
 import Component from "./components/Component.js";
 import Card from "./components/Card/Card.js";
 
-new Component(document.body, "main-container", "div");
-const mainDiv = document.querySelector(".main-container");
+new Component(document.body, "app container", "div");
+const mainDiv = document.querySelector(".app.container");
 
-new Component(mainDiv, "mainList", "ul");
-/* const cardList = document.querySelector(".mainList"); */
+new Component(mainDiv, "characters-list row list-unstyled", "ul");
+const cardList = document.querySelector(".characters-list.row.list-unstyled");
 
 /* new Component(cardList, "cardList", "li"); */
 
-new Card(mainDiv);
+new Card(cardList);
